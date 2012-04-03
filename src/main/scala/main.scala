af916c2601d4ffc3cff77f585a09d05aaca0907d
Alex Wilson <alex.wilson@navetas.com>
2012-04-03 22:28:59 +0100

    Routing.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -20,20 +20,20 @@ object EntityType extends Enumeration
 {
     import java.awt.Color
     
-    class Element( val name : String, val closed : Boolean, val color : java.awt.Color, val dashPattern : Option[Array[Float]] = None ) extends Val(name)
+    class Element( val name : String, val closed : Boolean, val routeable : Boolean, val color : java.awt.Color, val dashPattern : Option[Array[Float]] = None ) extends Val(name)
     
-    val unknown         = new Element("Unknown",        false, new Color( 0.7f, 0.4f, 0.7f ))
-    val highway         = new Element("Highway",        false, new Color( 0.3f, 0.0f, 0.0f ))
-    val cycleway        = new Element("Cycleway",       false, new Color( 0.0f, 1.0f, 0.0f ), Some( Array( 3.0f, 3.0f ) ))
-    val footpath        = new Element("Footpath",       false, new Color( 0.0f, 0.0f, 1.0f ), Some( Array( 3.0f, 3.0f ) ))
-    val railway         = new Element("Railway",        false, new Color( 0.0f, 0.0f, 0.0f ), Some( Array( 4.0f, 4.0f ) ))
-    val unknownLine     = new Element("Unknown line",   false, new Color( 0.7f, 0.7f, 0.7f ))
+    val unknown         = new Element("Unknown",        false, false, new Color( 0.7f, 0.4f, 0.7f ))
+    val highway         = new Element("Highway",        false, true,  new Color( 0.3f, 0.0f, 0.0f ))
+    val cycleway        = new Element("Cycleway",       false, true,  new Color( 0.0f, 1.0f, 0.0f ), Some( Array( 3.0f, 3.0f ) ))
+    val footpath        = new Element("Footpath",       false, true,  new Color( 0.0f, 0.0f, 1.0f ), Some( Array( 3.0f, 3.0f ) ))
+    val railway         = new Element("Railway",        false, false, new Color( 0.0f, 0.0f, 0.0f ), Some( Array( 4.0f, 4.0f ) ))
+    val unknownLine     = new Element("Unknown line",   false, false, new Color( 0.7f, 0.7f, 0.7f ))
     
-    val building        = new Element("Building",       true,  new Color( 0.9f, 0.9f, 0.9f ))
-    val woodland        = new Element("Woodland",       true,  new Color( 0.0f, 0.6f, 0.0f ))
-    val waterway        = new Element("Waterway",       true,  new Color( 0.5f, 0.5f, 1.0f ))
-    val greenspace      = new Element("Greenspace",     true,  new Color( 0.0f, 1.0f, 0.0f ))
-    val farmland        = new Element("Farmland",       true,  new Color( 0.5f, 0.3f, 0.3f ))
+    val building        = new Element("Building",       true,  false, new Color( 0.9f, 0.9f, 0.9f ))
+    val woodland        = new Element("Woodland",       true,  false, new Color( 0.0f, 0.6f, 0.0f ))
+    val waterway        = new Element("Waterway",       true,  false, new Color( 0.5f, 0.5f, 1.0f ))
+    val greenspace      = new Element("Greenspace",     true,  false, new Color( 0.0f, 1.0f, 0.0f ))
+    val farmland        = new Element("Farmland",       true,  false, new Color( 0.5f, 0.3f, 0.3f ))
 }
 
 object TestRunner extends App
@@ -65,8 +65,16 @@ object TestRunner extends App
         var nodes = mutable.ArrayBuffer[Node]()
     }
     
-    class Canvas( val nodes : Iterable[Node], val ways : Iterable[Way] ) extends JFrame
+    class Canvas( val nodes : Iterable[Node], val ways : Iterable[Way], val routeGraph : RouteGraph ) extends JFrame
     {
+        private def mouseClickHandler( fn : PInputEvent => Unit ) = new PBasicInputEventHandler()
+        {
+            override def mousePressed( event : PInputEvent ) = fn( event )
+            override def mouseDragged( event : PInputEvent ) {}
+            override def mouseReleased( event : PInputEvent ) {}
+            override def keyPressed( event : PInputEvent ) {}
+        }
+    
         val jp = javax.swing.Box.createHorizontalBox()
         add(jp)
         val canvas = new PCanvas()        
@@ -85,6 +93,9 @@ object TestRunner extends App
         sp.setMinimumSize( new java.awt.Dimension( 250, 800 ) )
         sp.setPreferredSize( new java.awt.Dimension( 250, 800 ) )
         jp.add( sp )
+        
+        var routeEndPoints = List[(PPath, Node)]()
+        var nodeToPathMap = mutable.HashMap[Node, PPath]()
         
         val layered = mutable.ListBuffer[(Int, PPath, Taggable)]()
         for ( w <- ways )
@@ -141,17 +152,13 @@ object TestRunner extends App
                 canvas.getLayer().addChild(label)
             }
             
-            n.addInputEventListener( new PBasicInputEventHandler()
+            
+            
+            n.addInputEventListener( mouseClickHandler( event =>
             {
-                override def mousePressed( event : PInputEvent )
-                {
-                    tm.data = t.keys.toArray.map( x => Array( x._1, x._2 ) )
-                    tm.fireTableDataChanged()
-                }
-                override def mouseDragged( event : PInputEvent ) {}
-                override def mouseReleased( event : PInputEvent ) {}
-                override def keyPressed( event : PInputEvent ) {}
-            } )
+                tm.data = t.keys.toArray.map( x => Array( x._1, x._2 ) )
+                tm.fireTableDataChanged()
+            } ) )
         }
         
         for ( node <- nodes.view.filter( !_.inWay ) )
@@ -170,19 +177,45 @@ object TestRunner extends App
                 canvas.getLayer().addChild(label)
             }
             
-            circ.addInputEventListener( new PBasicInputEventHandler()
+            circ.addInputEventListener( mouseClickHandler( event =>
             {
-                override def mousePressed( event : PInputEvent )
-                {
-                    tm.data = node.keys.toArray.map( x => Array( x._1, x._2 ) )
-                    tm.fireTableDataChanged()
-                }
-                override def mouseDragged( event : PInputEvent ) {}
-                override def mouseReleased( event : PInputEvent ) {}
-                override def keyPressed( event : PInputEvent ) {}
-            } )
+                tm.data = node.keys.toArray.map( x => Array( x._1, x._2 ) )
+                tm.fireTableDataChanged()
+            } ) )
         }
         
+        for ( node <- routeGraph.nodes )
+        {
+            val circ = PPath.createEllipse( node.lon.toFloat-3.0f, (-node.lat.toFloat)-3.0f, 6.0f, 6.0f )
+            circ.setPaint( new java.awt.Color( 0.0f, 0.0f, 1.0f ) )
+            canvas.getLayer().addChild(circ)
+            
+            nodeToPathMap += node -> circ
+            
+            circ.addInputEventListener( mouseClickHandler( event =>
+            {
+                if ( routeEndPoints.size >= 2 )
+                {
+                    routeEndPoints.foreach( c => c._1.setPaint( new java.awt.Color( 0.0f, 0.0f, 1.0f ) ) )
+                    routeEndPoints = Nil
+                }
+                routeEndPoints = (circ, node) :: routeEndPoints
+                circ.setPaint( new java.awt.Color( 1.0f, 0.0f, 0.0f ) )
+                
+                if ( routeEndPoints.size == 2 )
+                {
+                    val List((c1, n1), (c2, n2)) = routeEndPoints
+                    
+                    val routeNodes = routeGraph.shortestRoute( n1, n2 )
+                    val routeCircs = routeNodes.map( n => nodeToPathMap(n) )
+                    (routeCircs zip routeNodes).foreach( x =>
+                    {
+                        x._1.setPaint( new java.awt.Color( 0.0f, 1.0f, 0.0f ) )
+                        routeEndPoints = x :: routeEndPoints
+                    } )
+                }
+            } ) )
+        }
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
         setSize(1000, 700)
@@ -305,11 +338,56 @@ object TestRunner extends App
     }
     
     
-    class GenerateRouteGraph( xf : XMLFilter )
+    class RouteGraph( xf : XMLFilter )
     {
         type GraphT = SimpleWeightedGraph[Node, DefaultWeightedEdge]
         
         val graph = new GraphT(classOf[DefaultWeightedEdge])
+        
+        private def nodeDist( n1 : Node, n2 : Node ) =
+        {
+            import scala.math._
+
+            val theta = n1.lon - n2.lon
+            var dist = sin(toRadians(n1.lat)) * sin(toRadians(n2.lat)) + cos(toRadians(n1.lat)) * cos(toRadians(n2.lat)) * cos(toRadians(theta))
+            dist = acos(dist)
+            dist = toDegrees(dist)
+            dist = dist * 60 * 1.1515
+            dist = dist * 1.609344
+            
+            dist
+        }
+        
+        def nodes =
+        {
+            import scala.collection.JavaConverters._
+            graph.vertexSet().asScala
+        }
+        
+        def shortestRoute( n1 : Node, n2 : Node ) =
+        {
+            import scala.collection.JavaConverters._
+            
+            /*var edges = List[DefaultWeightedEdge]()
+            for ( i <- 0 until 10000 )
+            {
+                if ( (i % 100) == 0 ) println(i)
+                val dijk = new org.jgrapht.alg.DijkstraShortestPath( graph, n1, n2 )
+                edges = dijk.getPath().getEdgeList().asScala.toList
+            }*/
+            
+            val dijk = new org.jgrapht.alg.DijkstraShortestPath( graph, n1, n2 )
+            val edges = dijk.getPath().getEdgeList().asScala.toList
+            
+            var allNodes = immutable.HashSet[Node]()
+            for ( e <- edges )
+            {
+                allNodes += graph.getEdgeSource(e)
+                allNodes += graph.getEdgeTarget(e)
+            }
+            
+            allNodes
+        }
 
         // When generating the routing graph, we are interested in keeping
         // nodes that are at the start or end of a way, or in more than one
@@ -318,40 +396,43 @@ object TestRunner extends App
         def processWay( way : Way )
         {
             val lastIndex = way.nodes.length-1
-            val routeNodes = way.nodes.zipWithIndex.filter
+            var dist = 0.0
+            var lastNode : Option[Node] = None
+            var lastRouteNode : Option[Node] = None
+            for ( (n, i) <- way.nodes.zipWithIndex )
             {
-                case (n, i) => n.wayMembership > 1 || i==0 || i==lastIndex
-            }.map( _._1 )
-            
-            (routeNodes, routeNodes drop 1).zipped.foreach
-            {
-                case (first, second) =>
+                // Update running distance between route nodes
+                lastNode.foreach( ln => dist += (nodeDist(n, ln) * (if (way.entityType == EntityType.highway) 2.0 else 1.0)) )
+                
+                // Build route node if required
+                val isRouteNode = n.wayMembership > 1 || i==0 || i==lastIndex
+                if ( isRouteNode )
                 {
-                    // No penalty to adding vertices more than once
-                    graph.addVertex(first)
-                    graph.addVertex(second)
+                    graph.addVertex(n)
                     
-                    val e = graph.addEdge( first, second )
-                    graph.setEdgeWeight(e, 1.0)
+                    lastRouteNode.foreach( lrn =>
+                    {
+                        if ( lrn != n )
+                        {
+                            val e = graph.addEdge( lrn, n )
+                            if ( e != null ) graph.setEdgeWeight(e, dist)
+                        }
+                    } )
+                    dist = 0.0
+                    lastRouteNode = Some(n)
                 }
+                
+                lastNode = Some(n)
             }
         }
 
         // Build the routing graph, way-by-way
-        {
-            for ( w <- xf.ways )
-            {
-                if ( w.entityType == !w.nodes.isEmpty )
-                {
-                    w.entityType match
-                    {
-                        case (EntityType.highway|EntityType.cycleway|EntityType.footpath) => processWay(w)
-                        case _ =>
-                    }
-                }
-            }
-        }
+        for ( w <- xf.ways if w.entityType.routeable && w.nodes.length > 1 ) processWay(w)
+        println( "Num nodes: " + graph.vertexSet().size() + ", num edges: " + graph.edgeSet().size() )
     }
+    
+    //l = new LatLng(33.109283, -123.182312);
+
         
     override def main( args : Array[String] ) =
     {
@@ -364,7 +445,8 @@ object TestRunner extends App
         // West Chilterns
         //val f = new XMLFilter( args(0), new Bounds(-1.0436, -0.8356, 51.5668, 51.6656) )
         
-        val c = new Canvas( f.nodes.view.map( _._2 ), f.ways )
+        val rg = new RouteGraph( f )
+        val c = new Canvas( f.nodes.view.map( _._2 ), f.ways, rg )
     }
 }
 
